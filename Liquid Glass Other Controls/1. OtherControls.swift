107c37f12de8e528cd67dd1c693b9db4c878dc43
Stewart Lynch <slynch@createchsol.com>
2025-08-01 14:40:45 -0700

    Initial Commit

---
+++ Liquid Glass Other Controls/1. OtherControls.swift
@@ -0,0 +1,50 @@
+//
+//----------------------------------------------
+// Original project: Liquid Glass Other Controls
+// by  Stewart Lynch on 2025-07-27
+//
+// Follow me on Mastodon: https://iosdev.space/@StewartLynch
+// Follow me on Threads: https://www.threads.net/@stewartlynch
+// Follow me on Bluesky: https://bsky.app/profile/stewartlynch.bsky.social
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Email: slynch@createchsol.com
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2025 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct OtherControls: View {
+    var body: some View {
+        NavigationStack {
+            Form {
+                // Toggle
+               
+                LabeledContent("Segmented Control") {
+                    
+                }
+                
+                Section("Sliders and Ticks") {
+                    LabeledContent("Slider 1") {
+                       
+                    }
+                    LabeledContent("Slider 1") {
+                        
+                    }
+                
+                    LabeledContent("Slider 2") {
+                        
+                    }
+                }
+            }
+            .navigationTitle("Liquid Glass Controls")
+        }
+    }
+}
+
+#Preview {
+    OtherControls()
+}
---
+++ Liquid Glass Other Controls/3. PeopleListView.swift
@@ -0,0 +1,35 @@
+//
+//----------------------------------------------
+// Original project: Liquid Glass Other Controls
+// by  Stewart Lynch on 2025-07-29
+//
+// Follow me on Mastodon: https://iosdev.space/@StewartLynch
+// Follow me on Threads: https://www.threads.net/@stewartlynch
+// Follow me on Bluesky: https://bsky.app/profile/stewartlynch.bsky.social
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Email: slynch@createchsol.com
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2025 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct PeopleListView: View {
+    @State private var manager = DataManager()
+    var body: some View {
+        NavigationStack {
+            List(manager.people.sorted(), id: \.self) { person in
+                Text(person) 
+            }
+            .listStyle(.plain)
+                .navigationTitle("People List")
+        }
+    }
+}
+
+#Preview {
+    PeopleListView()
+}
